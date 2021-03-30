639f9b7179e482072f00cc3bf6beea9801183f9f
rry <r.yamakuchi@gmail.com>
2021-03-30 11:40:59 +0900

    add: no-use-before-define についてのルールを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,107 +1,110 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'plugin:react/recommended',
-        'airbnb',
-        'airbnb/hooks',
-        'plugin:import/errors',
-        'plugin:import/warnings',
-        'plugin:import/typescript',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:prettier/recommended',
-        'prettier',
+    ecmaVersion: 2021,
+    project: './tsconfig.eslint.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'jsx-a11y',
+    'prettier',
+    'react',
+    'react-hooks',
+  ],
+  root: true,
+  rules: {
+    'lines-between-class-members': [
+      'error',
+      'always',
+      {
+        exceptAfterSingleLine: true,
+      },
     ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-        jsx: true,
-        },
-        ecmaVersion: 2021,
-        project: './tsconfig.eslint.json',
-        sourceType: 'module',
-        tsconfigRootDir: __dirname,
-    },
-    plugins: [
-        '@typescript-eslint',
-        'import',
-        'jsx-a11y',
-        'prettier',
-        'react',
-        'react-hooks',
+    // should be rewritten as `['error', { allowAsStatement: true }]` in ESLint 7 or later
+    // SEE: https://github.com/typescript-eslint/typescript-eslint/issues/1184
+    'no-void': 'off',
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
+      },
     ],
-    root: true,
-    rules: {
-        'lines-between-class-members': [
-        'error',
-        'always',
-        {
-            exceptAfterSingleLine: true,
-        },
-        ],
-        // should be rewritten as `['error', { allowAsStatement: true }]` in ESLint 7 or later
-        // SEE: https://github.com/typescript-eslint/typescript-eslint/issues/1184
-        'no-void': 'off',
-        'padding-line-between-statements': [
-        'error',
-        {
-            blankLine: 'always',
-            prev: '*',
-            next: 'return',
-        },
-        ],
-        '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-            'vars': 'all',
-            'args': 'after-used',
-            'argsIgnorePattern': '_',
-            'ignoreRestSiblings': false,
-            'varsIgnorePattern': '_',
-        },
-        ],
-        'import/extensions': [
-        'error',
-        'ignorePackages',
-        {
-            js: 'never',
-            jsx: 'never',
-            ts: 'never',
-            tsx: 'never',
-        },
-        ],
-        'react/jsx-filename-extension': [
-        'error',
-        {
-            extensions: ['.jsx', '.tsx'],
-        },
-        ],
-        'react/jsx-props-no-spreading': [
-        'error',
-        {
-            html: 'enforce',
-            custom: 'enforce',
-            explicitSpread: 'ignore',
-        },
-        ],
-    },
-    overrides: [
-        {
-        'files': ['*.tsx'],
-        'rules': {
-            'react/prop-types': 'off',
-        },
-        },
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        argsIgnorePattern: '_',
+        ignoreRestSiblings: false,
+        varsIgnorePattern: '_',
+      },
     ],
-    settings: {
-        'import/resolver': {
-        node: {
-            paths: ['src'],
-        },
-        },
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'react/jsx-props-no-spreading': [
+      'error',
+      {
+        html: 'enforce',
+        custom: 'enforce',
+        explicitSpread: 'ignore',
+      },
+    ],
+    // https://github.com/typescript-eslint/typescript-eslint/issues/2540
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+      },
     },
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
 };
