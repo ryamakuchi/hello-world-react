9d9b1bc5462160480418fd22278e1a43278d7a9c
rry <r.yamakuchi@gmail.com>
2021-03-30 11:56:03 +0900

    add: prefer-arrow

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,7 @@ module.exports = {
     '@typescript-eslint',
     'import',
     'jsx-a11y',
+    'prefer-arrow',
     'prettier',
     'react',
     'react-hooks',
@@ -72,6 +73,14 @@ module.exports = {
         jsx: 'never',
         ts: 'never',
         tsx: 'never',
+      },
+    ],
+    'prefer-arrow/prefer-arrow-functions': [
+      'error',
+      {
+        disallowPrototype: true,
+        singleReturnOnly: false,
+        classPropertiesAllowed: false,
       },
     ],
     'react/jsx-filename-extension': [
